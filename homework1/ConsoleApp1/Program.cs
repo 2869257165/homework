dfd6139afdd785893881d07669d55b247ff1fcd6
TGW <2869257165@qq.com>
2018-09-10 20:55:45 +0800

    homwork1

---
+++ homework1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double a = 0;
+            double b = 0;
+            Console.WriteLine("please input first numble");
+            s = Console.ReadLine();
+            a = Double.Parse(s);
+            Console.WriteLine("please input second numble");
+            s = Console.ReadLine();
+            b = Double.Parse(s);
+            Console.WriteLine("The resurt is:" + (a * b) );
+        }
+    }
+}
