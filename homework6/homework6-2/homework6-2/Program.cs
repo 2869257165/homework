4592a3dc19cbd57388d6964150f25f7f2450212e
TGW <2869257165@qq.com>
2018-10-24 20:43:14 +0800

    homework6

---
+++ homework6/homework6-2/homework6-2/Program.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+namespace homework6_2
+{
+    [Serializable]
+    public class Order
+    {
+        private int ID;
+        private string CName;
+        public List<OrderDetail> Corderds = new List<OrderDetail>();
+        public Order() { }
+        public Order(int x, string y)
+        {
+            ID = x;
+            CName = y;
+        }
+        public void adddetail(OrderDetail p)
+        {
+            Corderds.Add(p);
+        }
+        public int getID
+        {
+            get
+            {
+                return ID;
+            }
+        }
+        public string getname
+        {
+            get
+            {
+                return CName;
+            }
+        }
+        public void showorder()
+        {
+            Console.WriteLine("订单号：" + ID);
+            Console.WriteLine("客户名：" + CName);
+            var m = from n in Corderds where n != null select n;
+            foreach (var i in m)
+            {
+                Console.WriteLine("商品名：" + i.getGName);
+                Console.WriteLine("商品数目" + i.getGNum);
+                Console.WriteLine("商品价格" + i.getGPri);
+            }
+        }
+    }
+    //订单信息
+    public class OrderDetail
+    {
+        private string GName;
+        private int GNum;
+        private double GPri;
+        public OrderDetail() { }
+        public OrderDetail(string gname, int gnum, int gpri)
+        {
+            GName = gname;
+            GNum = gnum;
+            GPri = gpri;
+        }
+        public string getGName
+        {
+            get
+            {
+                return GName;
+            }
+        }
+        public int getGNum
+        {
+            get
+            {
+                return GNum;
+            }
+        }
+        public double getGPri
+        {
+            get
+            {
+                return GPri;
+            }
+        }
+    }
+    public class MyOrderException : ApplicationException
+
+    {
+        private string information;
+        public MyOrderException(string message, string information) : base(message)
+        {
+            this.information = information;
+        }
+        public string getInformation()
+        {
+            return information;
+        }
+    }
+    //订单服务
+    public class OrderService
+    {
+        public static string xmlfilename = "sx.xml";
+        public XmlSerializer xmlser = new XmlSerializer(typeof(List<Order>));
+        public List<Order> orders = new List<Order>();
+        //初始化并储存订单信息
+        public OrderService()
+        {
+            Order mike = new Order(0, "mike");
+            mike.adddetail(new OrderDetail("adsf", 1000, 1));
+            mike.adddetail(new OrderDetail("jkl;", 2000, 2));
+            add(mike);
+            Order peter = new Order(1, "peter");
+            peter.adddetail(new OrderDetail("adsd", 3000, 5));
+            add(peter);
+            Order hh = new Order(2, "Tom");
+            hh.adddetail(new OrderDetail("adsf", 500, 6));
+            hh.adddetail(new OrderDetail("fgo", 700, 8));
+            hh.adddetail(new OrderDetail("uu", 900, 7));
+            add(hh);
+        }
+        //展示订单信息
+        public void showlist()
+        {
+            var m = from n in orders where n != null select n;
+            foreach (var i in m)
+            {
+                i.showorder();
+            }
+        }
+        //新增订单
+        public void add(Order p)
+        {
+            orders.Add(p);
+        }
+        public void Addorder()
+        {
+            Console.Write("订单号：");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("客户名：");
+            string name = Console.ReadLine();
+            Console.Write("商品名称：");
+            string gname = Console.ReadLine();
+            Console.Write("商品数目：");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("商品价格：");
+            int pri = int.Parse(Console.ReadLine());
+            OrderDetail temp = new OrderDetail(gname, num, pri);
+            Order torder = new Order(id, name);
+            torder.adddetail(temp);
+            orders.Add(torder);
+        }
+        //按订单号删除订单
+        public void deleteorder(int number)
+        {
+            bool t = false;
+            var m = from n in orders where n != null select n;
+            foreach (var i in m)
+            {
+                if (i.getID == number)
+                {
+                    t = true;
+                    orders.Remove(i);
+                    break;
+                }
+            }
+            if (t == false)
+            {
+                throw new MyOrderException("The order doesn't exit, you have to input a new ordernumber.", "remove wrong");
+            }
+        }
+        //按订单号修改订单
+        public void changeorders(int changeID, OrderDetail details)
+        {
+            bool t = false;
+            var m = from n in orders where n != null select n;
+            foreach (var i in m)
+            {
+                if (i.getID == changeID)
+                {
+                    t = true;
+                    orders[changeID - 1].adddetail(details);
+                }
+            }
+            if (t == false)
+            {
+                throw new MyOrderException("The order doesn't exit, you have to input a new ordernumber.", "changeorder wrong");
+            }
+        }
+        //按订单号查询订单
+        public bool finbyID(int number)
+        {
+            bool t = false;
+            var m = from n in orders where n != null select n;
+            foreach (var i in m)
+            {
+                if (i.getID == number)
+                {
+                    t = true;
+                    i.showorder();
+                    return t;
+                }
+            }
+            if (t == false)
+            {
+                return t;
+                throw new MyOrderException("The order doesn't exit, you have to input a new ordernumber.", "findByNumber wrong");
+            }
+            else
+                return t;
+        }
+        //按客户名查询订单号
+        public Order findbyname(string name)
+        {
+            bool t = false;
+            var m = from n in orders where n != null select n;
+            foreach (var i in m)
+            {
+                if (i.getname == name)
+                {
+                    t = true;
+                    i.showorder();
+                    return i;
+                }
+            }
+            if (t == false)
+            {
+                return null;
+                throw new MyOrderException("The order doesn't exit, you have to input a new ordernumber.", "findByName wrong");
+            }
+            else
+                return null;
+        }
+        public void output()
+        {
+            double tottalprice = 0;
+            var m = from n in orders where n != null select n;
+            foreach (var i in m)
+            {
+                var n = from j in i.Corderds where i.Corderds != null select j;
+                foreach (var k in n)
+                {
+                    tottalprice += (k.getGNum * k.getGPri);
+                }
+                if (tottalprice >= 10000)
+                {
+                    i.showorder();
+                }
+            }
+        }
+        //订单信息XML序列化
+        public void Export()
+        {
+            XmlSerialize(xmlser, xmlfilename, orders);
+        }
+        //反序列化载入
+        public void Import()
+        {
+            List<Order> orders2 = XmlDeserialize(xmlser, xmlfilename) as List<Order>;
+
+        }
+        public object XmlDeserialize(XmlSerializer ser, string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            object obj = ser.Deserialize(fs);
+            fs.Close();
+            return obj;
+        }
+        public void XmlSerialize(XmlSerializer ser, string filename, object obj)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            ser.Serialize(fs, obj);
+            fs.Close();
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            OrderService ptem = new OrderService();
+            ptem.Export();
+            ptem.Import();
+            Console.WriteLine("初始订单为");
+            ptem.showlist();
+            Console.WriteLine("其中订单总金额超过10000的为：");
+            ptem.output();
+            Console.WriteLine("输入需要删除的订单号：");
+            int i = int.Parse(Console.ReadLine());
+            try
+            {
+                ptem.deleteorder(i);
+            }
+            catch (MyOrderException e)
+            {
+                Console.WriteLine(e.getInformation());
+            }
+            Console.WriteLine("删除后为");
+            ptem.showlist();
+            Console.WriteLine("输入要查询的订单号");
+            i = int.Parse(Console.ReadLine());
+            try
+            {
+                ptem.finbyID(i);
+            }
+            catch (MyOrderException e)
+            {
+                Console.WriteLine(e.getInformation());
+            }
+            Console.WriteLine("输入要查询的客户名：");
+            string pname = Console.ReadLine();
+            try
+            {
+                ptem.findbyname(pname);
+            }
+            catch (MyOrderException e)
+            {
+                Console.WriteLine(e.getInformation());
+            }
+            Console.WriteLine("新增订单");
+            ptem.Addorder();
+            ptem.showlist();
+            Console.WriteLine("输入需要修改的订单号：");
+            i = int.Parse(Console.ReadLine());
+            Console.WriteLine("输入修改后的商品名：");
+            pname = Console.ReadLine();
+            Console.WriteLine("输入商品数目");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine("输入修改后的商品单价");
+            int p = int.Parse(Console.ReadLine());
+            OrderDetail x = new OrderDetail(pname, s, p);
+            ptem.changeorders(i, x);
+            ptem.showlist();
+        }
+    }
+}
---
+++ homework6/homework6-2/homework6-2Tests/OrderServiceTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework6_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework6_2.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void OrderServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void showlistTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void addTest()
+        {
+            OrderService pordser = new OrderService();
+            Order p = new Order(5,"make");
+            pordser.add(p);
+            Assert.AreEqual("make",pordser.orders[3].getname);
+        }
+
+        [TestMethod()]
+        public void AddorderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        //测试删除订单
+        public void deleteorderTest1()
+        {
+            OrderService pordser = new OrderService();
+            string name = pordser.orders[1].getname;
+            pordser.deleteorder(0);
+            Assert.AreEqual(name,pordser.orders[0].getname);
+        }
+
+        [TestMethod()]
+        public void changeordersTest()
+        {
+            OrderService pordser = new OrderService();
+            OrderDetail p = new OrderDetail("happy", 5, 6);
+            pordser.changeorders(1, p);
+            Assert.AreEqual(p.getGName, pordser.orders[1].Corderds[2].getGName);
+        }
+
+        [TestMethod()]
+        public void finbyIDTest()
+        {
+            OrderService pordser = new OrderService();
+            bool m=pordser.finbyID(1);
+            Assert.AreEqual(true, m);
+        }
+
+        [TestMethod()]
+        public void findbynameTest()
+        {
+            OrderService pordser = new OrderService();
+            Order p = pordser.findbyname("mike");
+            Assert.AreEqual(p.getname, pordser.orders[0].getname);
+
+        }
+
+        
+        
+        [TestMethod()]
+        public void ExportTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void XmlDeserializeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void XmlSerializeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
